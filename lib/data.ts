b0cb991f3d85cea6b3e9af1ff32fffd85f7236b7
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2026-05-17 21:51:19 +0100

    feat: initialize empty BIRTH_DATA and DEATH_DATA arrays in new data module

--- lib/data.ts
+++ lib/data.ts
@@ -1,29 +1,5 @@
 import type { BirthRecord, DeathRecord } from "./types";
 
-export const BIRTH_DATA: BirthRecord[] = [
-  { id: "2024/0147", fname: "ياسين", lname: "بنعلي", sex: "male", dob: "2024-03-15", father: "محمد", mother: "فاطمة الزهراء", office: "الرباط" },
-  { id: "2024/0148", fname: "مريم", lname: "العلوي", sex: "female", dob: "2024-03-16", father: "أحمد", mother: "خديجة", office: "الدار البيضاء" },
-  { id: "2024/0149", fname: "أيوب", lname: "المنصوري", sex: "male", dob: "2024-02-10", father: "عبد الله", mother: "سعاد", office: "فاس" },
-  { id: "2024/0150", fname: "سلمى", lname: "الحسني", sex: "female", dob: "2024-01-22", father: "يوسف", mother: "نعيمة", office: "مراكش" },
-  { id: "2023/0892", fname: "عمر", lname: "التازي", sex: "male", dob: "2023-11-05", father: "حسن", mother: "ليلى", office: "طنجة" },
-  { id: "2023/0893", fname: "هاجر", lname: "الإدريسي", sex: "female", dob: "2023-10-18", father: "إبراهيم", mother: "رقية", office: "الرباط" },
-  { id: "2023/0894", fname: "آدم", lname: "بوعزة", sex: "male", dob: "2023-09-30", father: "سعيد", mother: "حليمة", office: "الدار البيضاء" },
-  { id: "2023/0895", fname: "إيمان", lname: "الفاسي", sex: "female", dob: "2023-08-12", father: "خالد", mother: "زينب", office: "فاس" },
-  { id: "2022/0501", fname: "زكريا", lname: "الصفريوي", sex: "male", dob: "2022-06-25", father: "عمر", mother: "أمينة", office: "مراكش" },
-  { id: "2022/0502", fname: "أسماء", lname: "البكري", sex: "female", dob: "2022-05-14", father: "ياسر", mother: "سميرة", office: "طنجة" },
-  { id: "2022/0503", fname: "محمد", lname: "العراقي", sex: "male", dob: "2022-04-03", father: "عبد الرحمن", mother: "كريمة", office: "الرباط" },
-  { id: "2021/0320", fname: "نور", lname: "بنشقرون", sex: "female", dob: "2021-12-01", father: "رشيد", mother: "لطيفة", office: "الدار البيضاء" },
-];
+export const BIRTH_DATA: BirthRecord[] = [];
 
-export const DEATH_DATA: DeathRecord[] = [
-  { id: "و2024/0081", fname: "عبد الكريم", lname: "بنموسى", sex: "male", dod: "2024-04-02", father: "الحسين", mother: "رحمة", office: "الرباط" },
-  { id: "و2024/0082", fname: "فاطمة", lname: "الشرقاوي", sex: "female", dod: "2024-03-28", father: "عمر", mother: "عائشة", office: "الدار البيضاء" },
-  { id: "و2024/0083", fname: "محمد", lname: "الغزواني", sex: "male", dod: "2024-03-15", father: "يوسف", mother: "مليكة", office: "فاس" },
-  { id: "و2024/0084", fname: "خديجة", lname: "الناصري", sex: "female", dod: "2024-02-20", father: "أحمد", mother: "حنان", office: "مراكش" },
-  { id: "و2023/0612", fname: "حسن", lname: "الوافي", sex: "male", dod: "2023-12-10", father: "إبراهيم", mother: "نجاة", office: "طنجة" },
-  { id: "و2023/0613", fname: "زهرة", lname: "البوعناني", sex: "female", dod: "2023-11-25", father: "سعيد", mother: "فتيحة", office: "الرباط" },
-  { id: "و2023/0614", fname: "عبد الرحمن", lname: "الزياني", sex: "male", dod: "2023-10-08", father: "خالد", mother: "سعدية", office: "الدار البيضاء" },
-  { id: "و2023/0615", fname: "آمنة", lname: "المراكشي", sex: "female", dod: "2023-09-14", father: "رشيد", mother: "نادية", office: "فاس" },
-  { id: "و2022/0401", fname: "مصطفى", lname: "الفهري", sex: "male", dod: "2022-07-22", father: "عمر", mother: "بشرى", office: "مراكش" },
-  { id: "و2022/0402", fname: "حليمة", lname: "السوسي", sex: "female", dod: "2022-06-30", father: "حسن", mother: "لطيفة", office: "طنجة" },
-];
+export const DEATH_DATA: DeathRecord[] = [];
